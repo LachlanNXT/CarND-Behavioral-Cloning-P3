674f9c5edaf569741312b2d5eff77505483a7ceb
LachlanNXT <lachlan-27@hotmail.com>
2017-09-25 23:07:50 +0930

    added model.py

---
+++ model.py
@@ -0,0 +1,37 @@
+import csv
+import os
+import cv2
+
+lines = []
+with open('../P3_Data/driving_log.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		lines.append(line)
+		
+images = []
+measurements = []
+
+for line in lines:
+	source_path = line[0]
+	filename = os.path.basename(source_path)
+	current_path = os.path.join('../P3_Data/IMG/', filename)
+	image = cv2.imread(current_path)
+	images.append(image)
+	measurement = float(line[3])
+	measurements.append(measurement)
+	
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape = (160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split = 0.2, \\
+		  shuffle = True, nb_epoch=7)
+
+model.save('model.h5')
