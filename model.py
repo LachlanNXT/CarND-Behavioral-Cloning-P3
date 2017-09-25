030ffaf483067bff32cbce85bebb9a62991655ae
LachlanNXT <lachlan-27@hotmail.com>
2017-09-25 14:49:31 +0000

    model working on amazon EC2, saved first crappy version

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import csv
+import ntpath
 import os
-import cv2
 import numpy as np
+import matplotlib.image as mpimg
 
 lines = []
 with open('../P3_Data/driving_log.csv') as csvfile:
@@ -12,20 +13,25 @@ with open('../P3_Data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 
-for line in lines:
+for i,line in enumerate(lines):
 	source_path = line[0]
-	filename = os.path.basename(source_path)
+	filename = ntpath.basename(source_path)
 	current_path = os.path.join('../P3_Data/IMG/', filename)
 	try:
-		image = cv2.imread(current_path)
+		image = mpimg.imread(current_path)
+		#print("Reading Image: ",i)
 	except:
 		break
 	images.append(image)
 	measurement = float(line[3])
 	measurements.append(measurement)
-	
+print("read {} images".format(i))
+
 X_train = np.array(images)
 y_train = np.array(measurements)
+print("X_train shape: ", X_train.shape)
+print("y_train shape: ", y_train.shape)
+assert(len(X_train)==len(y_train))
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
@@ -35,7 +41,6 @@ model.add(Flatten(input_shape = (160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split = 0.2, \
-		  shuffle = True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch=7)
 
 model.save('model.h5')
